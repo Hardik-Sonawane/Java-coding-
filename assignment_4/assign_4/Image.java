462b1ea9dce29f3eea862172fecfea1f05a5bf5a
Hardik Sonawane <sonawanehardik080@gmail.com>
2025-03-10 15:48:26 +0530

    Update Image.java

--- assignment_4/assign_4/Image.java
+++ assignment_4/assign_4/Image.java
@@ -1,16 +1,16 @@
 package assign_4;
 
 public class Image {
-    // Protected multipliers allow subclasses to access these values.
+    
     protected int basePriceMultiplier = 5;
     protected int baseQualityMultiplier = 1;
 
-    // Method to display image price calculation.
+  
     public void type(int price) {
         System.out.println("The New image price is: " + (price * basePriceMultiplier));
     }
 
-    // Method to display image quality.
+    
     public void quality(int star) {
         System.out.println("The image quality is: " + (star * baseQualityMultiplier));
     }
