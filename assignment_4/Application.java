8b53deba2469743fe80ea7005176efe5710a03dd
Hardik Sonawane <sonawanehardik080@gmail.com>
2025-03-10 11:33:45 +0530

    Update Application.java

--- assignment_4/Application.java
+++ assignment_4/Application.java
@@ -1,5 +1,5 @@
-import assign_4.*;        
-import assign_4_2.*;      
+import assign_4.*;
+import assign_4_2.*;
 import java.util.Scanner;
 
 public class Application {
@@ -7,34 +7,27 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       
-        Image image = new Image();
-        Photo photo = new Photo(); 
+        // Create an object of Photo (which extends Image)
+        Photo photo = new Photo();
 
+        // Use the Image functionality via Photo's inherited methods.
         System.out.print("Enter price for image: ");
-        int price = Integer.parseInt(sc.nextLine());  
+        int price = Integer.parseInt(sc.nextLine());
+        photo.imageType(price);
 
-      
-        image.type(price);
-
-     
         System.out.print("Enter quality rating for image (1-10): ");
-        int star = Integer.parseInt(sc.nextLine());  
-        image.quality(star);
+        int star = Integer.parseInt(sc.nextLine());
+        photo.imageQuality(star);
 
-        
+        // Use the Photo-specific (overridden) methods.
         System.out.print("Enter price for photo: ");
-        int photoPrice = Integer.parseInt(sc.nextLine());  
-
-      
+        int photoPrice = Integer.parseInt(sc.nextLine());
         photo.type(photoPrice);
 
-        
         System.out.print("Enter quality rating for photo (1-10): ");
-        int photoStar = Integer.parseInt(sc.nextLine());  
+        int photoStar = Integer.parseInt(sc.nextLine());
         photo.quality(photoStar);
 
-        sc.close(); 
+        sc.close();
     }
 }
-
