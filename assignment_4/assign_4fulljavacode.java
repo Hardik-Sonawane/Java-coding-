4bd9beb7a4c0772fa988ab08d4761306e2be86b4
Hardik Sonawane <sonawanehardik080@gmail.com>
2025-03-11 22:29:55 +0530

    Update assign_4fulljavacode.java

--- assignment_4/assign_4fulljavacode.java
+++ assignment_4/assign_4fulljavacode.java
@@ -20,4 +20,35 @@ public class Image {
 
 // photo class code
 
+package assign_4_2;
 
+import assign_4.Image;
+
+public class Photo extends Image {
+
+    private int photoPriceMultiplier = 10;
+    private int photoQualityMultiplier = 5;
+
+
+    @Override
+    public void type(int price) {
+        System.out.println("The New photo price is: " + (price * photoPriceMultiplier));
+    }
+
+
+    @Override
+    public void quality(int star) {
+        System.out.println("The photo quality out of 50 is: " + (star * photoQualityMultiplier));
+    }
+
+
+    public void imageType(int price) {
+        super.type(price);
+    }
+
+    public void imageQuality(int star) {
+        super.quality(star);
+    }
+}
+
+// application class code
