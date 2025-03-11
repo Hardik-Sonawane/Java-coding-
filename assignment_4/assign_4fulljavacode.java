607021ba55736afe27564572aa21c4b0bbd05ac1
Hardik Sonawane <sonawanehardik080@gmail.com>
2025-03-11 22:26:16 +0530

    Update assign_4fulljavacode.java

--- assignment_4/assign_4fulljavacode.java
+++ assignment_4/assign_4fulljavacode.java
@@ -1,1 +1,21 @@
-. 
+// image class code 
+package assign_4;
+
+public class Image {
+
+    protected int basePriceMultiplier = 5;
+    protected int baseQualityMultiplier = 1;
+
+
+    public void type(int price) {
+        System.out.println("The New image price is: " + (price * basePriceMultiplier));
+    }
+
+
+    public void quality(int star) {
+        System.out.println("The image quality is: " + (star * baseQualityMultiplier));
+    }
+}
+
+
+
